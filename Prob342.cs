94e50a33ccef5ea67960ce7a1375cf293a74a916
Lillian <fleming.lillian@gmail.com>
2020-03-15 18:34:52 -0500

    Add project files.

---
+++ Prob342.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Prob342
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            //Given an integer(signed 32 bits), write a function to check whether it is a power of 4.
+                        
+            var baseNum = Math.Pow(num, (1.0 / 4.0));
+            var baseNumDifference = Math.Ceiling(baseNum) - Math.Floor(baseNum);
+            if (num == 0)
+            {
+                return false;
+            }
+            else if (baseNumDifference == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                       
+        }
+    }
+}
