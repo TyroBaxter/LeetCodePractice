386df420636a70267be54220616bf8a78f13f1d2
Lillian <fleming.lillian@gmail.com>
2020-03-19 22:16:47 -0500

    Beginnings of #1281

---
+++ LeetCode.Tests/Prob1281Test.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass]
+    public class Prob1281Test
+    {
+        [TestMethod]
+        public void It_should_return_15()
+        {
+            var p = new Prob1281();
+            var result = p.SubtractProductAndSum(234);
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void It_should_return_21()
+        {
+            var p = new Prob1281();
+            var result = p.SubtractProductAndSum(4421);
+
+            Assert.AreEqual(21, result);
+        }
+    }
+}
