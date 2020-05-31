413b3ebe50984d5eecac99b1c2851aca90d4f0db
Lillian <fleming.lillian@gmail.com>
2020-05-31 18:22:43 -0500

    finished out CountNumberSmallerThan()

--- LeetCode.Code/Prob1365.cs
+++ LeetCode.Code/Prob1365.cs
@@ -14,7 +14,31 @@ namespace LeetCode.Code
 
         public int[] SmallerNumbersThanCurrent(int[] nums)
         {
+            var numPlaceCount = nums.Length;
+            var newNumArray = new int[numPlaceCount];
 
+            var arrayIndex = 0;
+            foreach (var num in nums)
+            {
+                newNumArray[arrayIndex] = CountNumbersSmallerThan(nums, num);
+                arrayIndex++;
+            }
+            return newNumArray;
         }
+
+        private int CountNumbersSmallerThan(int[] nums, int num)
+        {
+            var counter = 0; 
+            foreach (var number in nums)
+            {
+                if (num > number)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+
     }
 }
