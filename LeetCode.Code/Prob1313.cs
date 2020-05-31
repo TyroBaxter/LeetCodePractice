05bc21c24962cfd988eb6a49978d248df2a0d558
Lillian <fleming.lillian@gmail.com>
2020-05-31 18:38:06 -0500

    unit tests added for prob1313

---
+++ LeetCode.Code/Prob1313.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code
+{
+    public class Prob1313
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+
+        }
+    }
+}
---
+++ LeetCode.Tests/Prob1313Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass]
+    public class Prob1313Test
+    {
+        //Input: nums = [1,2,3,4]
+        //Output: [2,4,4,4]
+        [TestMethod]
+        public void It_should_return_2444()
+        {
+            var p = new Prob1313();
+            var result = p.DecompressRLElist(new int[] { 1, 2, 3, 4 });
+
+            CollectionAssert.AreEquivalent(result, new int[] { 2, 4, 4, 4 });
+        }
+
+
+        //Input: nums = [1,1,2,3]
+        //Output: [1,3,3]
+        [TestMethod]
+        public void It_should_return_133()
+        {
+            var p = new Prob1313();
+            var result = p.DecompressRLElist(new int[] { 1, 1, 2, 3 });
+
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 3, 3 });
+        }
+    }
+}
