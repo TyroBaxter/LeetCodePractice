bca3983828a73318de75f447507265eaadf9f2f8
Lillian <fleming.lillian@gmail.com>
2020-05-31 19:38:26 -0500

    Beginning pseudocode and breaking tasks out into methods

--- LeetCode.Code/Prob1313.cs
+++ LeetCode.Code/Prob1313.cs
@@ -8,7 +8,40 @@ namespace LeetCode.Code
     {
         public int[] DecompressRLElist(int[] nums)
         {
+            //split up array into pairs
+            var pairArray = ChunkArrayIntoPairs(nums);
+
+            //first value is the pair assigned to 'frequency'
+            //second value in the pair is assigned to 'value'
+            //the result of each pair array will generate its own new array
+            var result = CreateResultArray(pairArray[0], pairArray[1]);
+
+            //all result arrays are then concatenated and returned as one final array 
+            return ConcatenateAllThePairArrays(result);
+        }
+
+        private int[] ChunkArrayIntoPairs(int[] sourceArray)
+        {
+            int[] buffer = null;
+
+            for (int i = 0; i < sourceArray.Length; i += 2)
+            {
+                buffer = new int[2];
+                Array.Copy(sourceArray, i, buffer, 0, 2);
+            }
+
+            return buffer;
+        }
+
+        private int[] CreateResultArray(int frequency, int value)
+        {
 
         }
+
+        private int[] ConcatenateAllThePairArrays(int[] args)
+        {
+
+        }
+
     }
 }
