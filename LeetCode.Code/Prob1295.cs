9d34fcc9fea8ad5a459236b8f36a6b7a68fc2cba
Lillian <fleming.lillian@gmail.com>
2020-06-04 21:48:09 -0500

    Methods for both while-loop and recursion solutions

--- LeetCode.Code/Prob1295.cs
+++ LeetCode.Code/Prob1295.cs
@@ -6,9 +6,38 @@ namespace LeetCode.Code
 {
     public class Prob1295
     {
+        //Given an array nums of integers, return how many of them contain an even number of digits.
         public int FindNumbers(int[] nums)
         {
+            var counter = 0;
 
+            foreach (var num in nums)
+            {
+                if (countDigitsRecursion(num) % 2 == 0)
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+
+        public static int countDigitsLongform(int n)
+        {
+            var counter = 0;
+            while (n > 0)
+            {
+                n = n / 10;
+                counter++;
+            }
+            return counter;
+        }
+        public static int countDigitsRecursion(int n)
+        {
+            if (n == 0)
+                return 0;
+            return 1 + countDigitsRecursion(n / 10);
         }
     }
 }
