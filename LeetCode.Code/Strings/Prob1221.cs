0fbd933dbf088f3d855f1386f94642adaac52592
Lillian <fleming.lillian@gmail.com>
2020-06-04 22:31:54 -0500

    Prob1221 structure and tests

---
+++ LeetCode.Code/Strings/Prob1221.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.Strings
+{
+    public class Prob1221
+    {
+        //Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+        //Given a balanced string s split it in the maximum amount of balanced strings.
+
+        //Return the maximum amount of splitted balanced strings.
+        public int BalancedStringSplit(string unbalancedInput)
+        {
+            //Loop from left to right maintaining a balance variable when it gets an L increase it by one 
+            //otherwise decrease it by one.
+            var balance = 0;
+            foreach (var s in unbalancedInput)
+            {
+                if (s.Equals('L'))
+                {
+                    balance++;
+                }
+                else
+                {
+                    balance--;
+                }
+            }
+            //Whenever the balance variable reaches zero then we increase the answer by one.
+        }
+    }
+}
---
+++ LeetCode.Tests/StringsTests/Prob1221Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Code.Strings;
+
+namespace LeetCode.Tests.StringsTests
+{
+    [TestClass]
+    public class Prob1221Test
+    {
+        [TestMethod]
+        public void It_should_return_4()
+        {
+            //Input: s = "RLRRLLRLRL"
+            //Output: 4
+            //Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+            var p = new Prob1221();
+
+            var result = p.BalancedStringSplit("RLRRLLRLRL");
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void It_should_return_3()
+        {
+            //Input: s = "RLLLLRRRLR"
+            //Output: 3
+            //Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+            var p = new Prob1221();
+
+            var result = p.BalancedStringSplit("RLLLLRRRLR");
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void It_should_return_1()
+        {
+            //Input: s = "LLLLRRRR"
+            //Output: 1
+            //Explanation: s can be split into "LLLLRRRR".
+            var p = new Prob1221();
+
+            var result = p.BalancedStringSplit("LLLLRRRR");
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void It_should_return_2()
+        {
+            //Input: s = "RLRRRLLRLL"
+            //Output: 2
+            //Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+            var p = new Prob1221();
+
+            var result = p.BalancedStringSplit("RLRRRLLRLL");
+            Assert.AreEqual(result, 2);
+        }
+    }
+}
