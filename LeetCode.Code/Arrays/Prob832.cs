b0ad07dd2175a4a079fef8df6cc2c7e5790240db
Lillian <fleming.lillian@gmail.com>
2020-06-15 22:44:10 -0500

    Adding more methods

--- LeetCode.Code/Arrays/Prob832.cs
+++ LeetCode.Code/Arrays/Prob832.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace LeetCode.Code.Arrays
 {
     public class Prob832
@@ -18,6 +19,64 @@ namespace LeetCode.Code.Arrays
         public int[][] FlipAndInvertImage(int[][] A)
         {
 
+            //Get the data into a better format 
+            var betterData = new List<List<int>>();
+            
+            for (int i = 0; i < A.Length; i++)
+            {
+                int[] innerDataFromInput = A[i];
+                var chunkedData = new List<int>(innerDataFromInput);
+             
+                betterData.Add(chunkedData);
+            }
+
+            //reverse the data 
+            ReverseTheData(betterData);
+            //flip the 0s and 1s
+            FlipAllTheDigits(betterData);
+            //turn it back into a jagged array and return it
+            var flipItAndInvertIt = new int[A.Length][];
+            //for (int i = 0; i < betterData.Count; i++)
+            //{
+            //    List<int> innerDataFromBetterData = betterData[i];
+            //    var chunkedData = new int[innerDataFromBetterData.Count];
+            //    foreach (var item in innerDataFromBetterData)
+            //    {
+            //        chunkedData.SetValue(item, )
+            //    }
+            //}
+            for (int i = 0; i < betterData.Count; i++)
+            {
+                List<int> innerDataFromBetterData = betterData[i];
+                flipItAndInvertIt[i] = innerDataFromBetterData.ToArray();
+
+            }
+            return flipItAndInvertIt;
+        }
+
+        private void ReverseTheData(List<List<int>> data)
+        {
+            data.ForEach(x => x.Reverse());
+        }
+
+        private void FlipAllTheDigits(List<List<int>> input)
+        {
+            input.ForEach(x => FlipSomeOfTheDigits(x));
+        }
+
+        private void FlipSomeOfTheDigits(List<int> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == 1)
+                {
+                    input[i] = 0;
+                }
+                else
+                {
+                    input[i] = 1;
+                }
+            }
         }
     }
 }
