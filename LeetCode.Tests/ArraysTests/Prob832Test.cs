60195c560e87f2fb8ebf6a1e42cfbc11993e534b
Lillian <fleming.lillian@gmail.com>
2020-06-15 20:39:26 -0500

    Outline Prob832 and unit tests

---
+++ LeetCode.Tests/ArraysTests/Prob832Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Code.Arrays;
+
+namespace LeetCode.Tests.ArraysTests
+{
+    [TestClass]
+    public class Prob832Test
+    {
+        [TestMethod]
+        public void It_should_return_100_010_111()
+        {
+            //Input: [[1, 1, 0],[1,0,1],[0,0,0]]
+            //Output: [[1,0,0],[0,1,0],[1,1,1]]
+            //Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+            //Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+            var p = new Prob832();
+
+            var result = p.FlipAndInvertImage(new int[][] {
+                new int [] { 1, 1, 0},
+                new int [] { 1, 0, 1},
+                new int [] { 0, 0, 0}
+            });
+
+            CollectionAssert.AreEquivalent(result, new int[][]
+            {
+                new int[] {1,0,0},
+                new int[] {0,1,0},
+                new int[] {1,1,1}
+            });
+        }
+
+        [TestMethod]
+        public void It_should_return_1100_0110_0001_1010()
+        {
+            //Input: [[1, 1, 0, 0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+            //Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+            var p = new Prob832();
+
+            var result = p.FlipAndInvertImage(new int[][]
+            {
+                new int[]{1,1,0,0},
+                new int[]{1,0,0,1},
+                new int[]{0,1,1,1},
+                new int[]{1,0,1,0}
+            });
+
+            CollectionAssert.AreEquivalent(result, new int[][]
+            {
+                new int[]{1,1,0,0},
+                new int[]{0,1,1,0},
+                new int[]{0,0,0,1},
+                new int[]{1,0,1,0}
+            });
+        }
+    }
+}
