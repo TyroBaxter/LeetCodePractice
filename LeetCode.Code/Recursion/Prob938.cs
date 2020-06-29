bbd7635db50126a70bd2f4acfa5cbb3ec32ada45
Lillian <fleming.lillian@gmail.com>
2020-06-28 19:01:08 -0500

    Gonna skip this one for now

---
+++ LeetCode.Code/Recursion/Prob938.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace LeetCode.Code.Recursion
+//{
+
+//    //Definition for a binary tree node.
+//    public class TreeNode
+//    {
+//        public int val;
+//        public TreeNode left;
+//        public TreeNode right;
+//        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+//        {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+
+//    //Given the root node of a binary search tree, return the sum of values 
+//    //of all nodes with value between L and R(inclusive).
+
+//    //The binary search tree is guaranteed to have unique values.
+//    public class Prob938
+//    {
+//        public int RangeSumBST(TreeNode root, int L, int R)
+//        {
+
+//        }
+//    }
+//}
---
+++ LeetCode.Tests/RecursionTests/Prob938Test.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using LeetCode.Code.Recursion;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace LeetCode.Tests.RecursionTests
+//{
+//    [TestClass]
+//    public class Prob938Test
+//    {
+//        //Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+//        //Output: 32
+
+//        [TestMethod]
+//        public void It_should_return_32()
+//        {
+//            var p = new Prob938();
+
+//            var result = p.RangeSumBST(new TreeNode[10, 5, 15, 3, 7, null, 18], 7, 15);
+
+//            Assert.AreEqual(result, 32);
+//        }
+
+//        //Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+//        //Output: 23
+//        [TestMethod]
+//        public void It_should_return_23()
+//        {
+//            var p = new Prob938();
+
+//            var result = p.RangeSumBST(new TreeNode([10, 5, 15, 3, 7, 13, 18, 1, null, 6], 6, 10));
+
+//            Assert.AreEqual(result, 23);
+//        }
+//    }
+//}
