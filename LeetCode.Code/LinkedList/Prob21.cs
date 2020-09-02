5cab6e6e8bd3539f061ccf2e7ee2eda8257803f9
Lillian <fleming.lillian@gmail.com>
2020-09-02 07:48:50 -0500

    Prob21 TakeTwo, with commentary!

--- LeetCode.Code/LinkedList/Prob21.cs
+++ LeetCode.Code/LinkedList/Prob21.cs
@@ -36,5 +36,63 @@ namespace LeetCode.Code.LinkedList
 
             return mergedInput;
         }
+
+        public ListNode MergeTwoListsTakeTwo(ListNode currentFirstInput, ListNode currentSecondInput)
+        {
+            var newHead = new ListNode(-1);
+            var currentPosition = newHead;
+
+            //initial guard-clause null check
+            while (currentFirstInput != null && currentSecondInput != null)
+            {
+
+                if (currentFirstInput.val <= currentSecondInput.val)
+                {
+                    currentPosition.next = currentFirstInput;
+                    currentFirstInput = currentFirstInput.next;
+                }
+                else
+                {
+                    currentPosition.next = currentSecondInput;
+                    currentSecondInput = currentSecondInput.next;
+                }
+
+                currentPosition = currentPosition.next;
+            }
+
+            while (currentFirstInput != null)
+            {
+                currentPosition.next = currentFirstInput;
+                currentPosition = currentPosition.next;
+                currentFirstInput = currentFirstInput.next;
+            }
+
+            while (currentSecondInput != null)
+            {
+                currentPosition.next = currentSecondInput;
+                currentPosition = currentPosition.next;
+                currentSecondInput = currentSecondInput.next;
+            }
+
+            //this assigns the pointer from newHead which will then follow the new 'chain'
+            //that currentPosition created as it reassigned pointers while traversing
+            //along currentFirstInput and currentSecondInput
+            var mergedInputResult = newHead.next;
+
+            //This then destroys the linkage newHead has with the variables that exist outside the method.
+            //This is done to prevent memory leakage, as a temporary variable internal to a method should not
+            //persist outside of that method. This is done as a Best Practice and is not related to the functionality
+            //of the method. It is also probable that the C# garbage collector will take care of this on its own anyway,
+            //but better to be safe than sorry. 
+            newHead.next = null;
+
+            return mergedInputResult;
+
+
+
+
+
+
+        }
     }
 }
