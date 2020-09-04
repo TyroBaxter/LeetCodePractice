7caf1358a271cf8a4bbf03daa28bc6fecd6b49e0
Lillian <fleming.lillian@gmail.com>
2020-09-04 07:54:02 -0500

    Prob1290 unit tests

---
+++ LeetCode.Code/LinkedList/Prob1290.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.LinkedList
+{
+    public class Prob1290
+    {
+        //    Given head which is a reference node to a singly-linked list.
+        //   The value of each node in the linked list is either 0 or 1. 
+        //   The linked list holds the binary representation of a number.
+
+        //    Return the decimal value of the number in the linked list.
+        public int GetDecimalValue(ListNode head)
+        {
+            //traverse the linked list, hit next until it is null 
+
+            //while traversing the linked list, place each .val in a list 
+
+            //convert binary to decimal (??) 
+
+            return 0;
+        }
+    }
+}
---
+++ LeetCode.Tests/LinkedListTests/Prob1290Test.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode.Code.LinkedList;
+using LeetCode.Code.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.LinkedListTests
+{
+    [TestClass]
+    public class Prob1290Test
+    {
+        [TestMethod]
+        public void It_should_return_5()
+        {
+            var p = new Prob1290();
+
+            var result = p.GetDecimalValue(new[]
+            {
+                1
+            }.ToLinkedList());
+
+            Assert.AreEqual(result, 5);
+
+        }
+
+        [TestMethod]
+        public void It_should_return_0()
+        {
+            var p = new Prob1290();
+
+            var result = p.GetDecimalValue(new[]
+            {
+                0
+            }.ToLinkedList());
+
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void It_should_return_1880()
+        {
+            var p = new Prob1290();
+
+            var result = p.GetDecimalValue(new[]
+            {
+                1,0,0,1,0,0,1,1,1,0,0,0,0,0,0
+            }.ToLinkedList());
+
+            Assert.AreEqual(result, 1880);
+        }
+
+
+
+    }
+}
