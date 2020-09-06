e481020b9921b1ddeca6d86bdfb2407024408557
Lillian <fleming.lillian@gmail.com>
2020-09-06 18:29:29 -0500

    Prob83 tests

---
+++ LeetCode.Code/LinkedList/Prob83.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.LinkedList
+{
+    public class Prob83
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode.Tests/LinkedListTests/Prob83Test.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Code.Extensions;
+using LeetCode.Code.LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.LinkedListTests
+{
+    [TestClass]
+    public class Prob83Test
+    {
+        [TestMethod]
+        public void It_should_return_1_2()
+        {
+            var p = new Prob83();
+            var result = p.DeleteDuplicates(new[]
+            {
+                1,1,2
+            }.ToLinkedList());
+
+            Assert.AreEqual(result, new[] {1,2 }.ToLinkedList() );
+        }
+
+        [TestMethod]
+        public void It_should_return_1_2_3()
+        {
+            var p = new Prob83();
+            var result = p.DeleteDuplicates(new[]
+            {
+                1,1,2,3,3
+            }.ToLinkedList());
+
+            Assert.AreEqual(result, new[] {1,2,3}.ToLinkedList());
+        }
+    }
+}
