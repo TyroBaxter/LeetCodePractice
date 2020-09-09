24ca9582e1fe586cb4e0e36c671fb67df0cf1340
Lillian <fleming.lillian@gmail.com>
2020-09-09 07:38:10 -0500

    Filling out unit tests

--- LeetCode.Code/Arrays/Prob1480.cs
+++ LeetCode.Code/Arrays/Prob1480.cs
@@ -8,10 +8,12 @@ namespace LeetCode.Code.Arrays
     {
         //Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
         //Return the running sum of nums.
+        //Input: nums = [1,2,3,4]
+        //Output: [1,3,6,10]
+        //Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
         public int[] RunningSum(int[] nums)
         {
-
-            return null; 
+          
         }
     }
 }
--- LeetCode.Tests/ArraysTests/Prob1480Test.cs
+++ LeetCode.Tests/ArraysTests/Prob1480Test.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using LeetCode.Code.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,13 +12,19 @@ namespace LeetCode.Tests.ArraysTests
         [TestMethod]
         public void It_should_return_1_3_6_10()
         {
+            var p = new Prob1480();
+            var result = p.RunningSum(new int[] { 1, 2, 3, 4 });
 
+            CollectionAssert.AreEquivalent(result, new int[] { 1, 3, 6, 10 });
         }
 
         [TestMethod]
         public void It_should_return_1_2_3_4_5()
         {
+            var p = new Prob1480();
+            var result = p.RunningSum(new int[]{1,1,1,1,1});
 
+            CollectionAssert.AreEquivalent(result, new int[] {1,2,3,4,5 });
         }
     }
 }
