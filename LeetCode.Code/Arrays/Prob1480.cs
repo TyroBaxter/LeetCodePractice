447ada37d687fc9d135174de80951cfdbd815c70
Lillian <fleming.lillian@gmail.com>
2020-09-14 07:29:06 -0500

    Prob1480

--- LeetCode.Code/Arrays/Prob1480.cs
+++ LeetCode.Code/Arrays/Prob1480.cs
@@ -13,6 +13,17 @@ namespace LeetCode.Code.Arrays
         //Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
         public int[] RunningSum(int[] nums)
         {
+            if (nums.Length == 1)
+            {
+                return nums;
+            }
+            //to reduce space complexity, operating on the input int[] nums itself
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i-1];
+            }
+
+            return nums;
           
         }
     }
