0c38c72751b9ee418b569545fc25d1e431067d19
Lillian <fleming.lillian@gmail.com>
2020-09-14 07:43:26 -0500

    Set up the tests for Prob1431

---
+++ LeetCode.Code/Arrays/Prob1431.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.Arrays
+{
+    public class Prob1431
+    {
+        // Given the array candies and the integer extraCandies, 
+        // where candies[i] represents the number of candies that the ith kid has.
+
+        // For each kid check if there is a way to distribute extraCandies among the kids 
+        // such that he or she can have the greatest number of candies among them. 
+        // Notice that multiple kids can have the greatest number of candies.
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysTests/Prob1431Test.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.ArraysTests
+{
+    [TestClass]
+    public class Prob1431Test
+    {
+
+
+        [TestMethod]
+        public void It_should_return_true_true_true_false_true()
+        {
+            //Input: candies = [2,3,5,1,3], extraCandies = 3
+            //Output: [true,true,true,false,true] 
+        }
+
+
+
+        [TestMethod]
+        public void It_should_return_true_false_false_false_false()
+        {
+            //Input: candies = [4,2,1,1,2], extraCandies = 1
+            //Output: [true,false,false,false,false]
+        }
+
+        [TestMethod]
+        public void It_should_return_true_false_true()
+        {
+            ///Input: candies = [12,1,12], extraCandies = 10
+            //Output: [true,false,true]
+        }
+    }
+}
