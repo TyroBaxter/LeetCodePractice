4c1031343598f9fda4bfce4f27e94940c2d06cc4
Lillian <fleming.lillian@gmail.com>
2020-09-14 21:17:22 -0500

    Prob1512

--- LeetCode.Code/Arrays/Prob1512.cs
+++ LeetCode.Code/Arrays/Prob1512.cs
@@ -8,7 +8,20 @@ namespace LeetCode.Code.Arrays
     {
         public int NumIdenticalPairs(int[] nums)
         {
-            return 0;
+            var countPairs = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        countPairs++;
+                    }
+                }
+            }
+
+
+            return countPairs;
         }
     }
 }
