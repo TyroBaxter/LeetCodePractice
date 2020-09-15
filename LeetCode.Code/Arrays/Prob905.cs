9d75c1fe811ddeb0e7954868b04874fc82d39484
Lillian <fleming.lillian@gmail.com>
2020-09-14 21:25:12 -0500

    Stubbing out prob 905

---
+++ LeetCode.Code/Arrays/Prob905.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.Arrays
+{
+    public class Prob905
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysTests/Prob905Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Code.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysTests
+{
+    [TestClass]
+    public class Prob905Test
+    {
+        //Input: [3,1,2,4]
+        //Output: [2,4,3,1]
+        //The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+        [TestMethod]
+        public void It_should_return_odds_and_evens()
+        {
+            var p = new Prob905();
+            var actual = p.SortArrayByParity(new int[]{3,1,2,3});
+            var result = new int[] {4,2,1,3};
+
+            CollectionAssert.AreEquivalent(result, actual);
+        }
+    }
+}
