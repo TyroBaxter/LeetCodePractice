e6e5fa11b4568f707eaaff966244b8417ce418f5
Lillian <fleming.lillian@gmail.com>
2020-09-14 21:01:54 -0500

    Stubbing out unit tests for 1512

---
+++ LeetCode.Code/Arrays/Prob1512.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.Arrays
+{
+    public class Prob1512
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysTests/Prob1512Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Code.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests.ArraysTests
+{
+    [TestClass]
+    public class Prob1512Test
+    {
+        //Input: nums = [1,2,3,1,1,3]
+        //Output: 4
+        //Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+        [TestMethod]
+        public void It_should_return_4()
+        {
+            var p = new Prob1512();
+            var result = p.NumIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 });
+            var actual = 4;
+
+            Assert.AreEqual(result, actual);
+        }
+
+        //Input: nums = [1,1,1,1]
+        //Output: 6
+        //Explanation: Each pair in the array are good.
+        [TestMethod]
+        public void It_should_return_6()
+        {
+            var p = new Prob1512();
+            var result = p.NumIdenticalPairs(new int[] {1,1,1,1});
+            var actual = 6;
+
+            Assert.AreEqual(actual, result);
+        }
+
+        //Input: nums = [1,2,3]
+        //Output: 0
+        [TestMethod]
+        public void It_should_return_0()
+        {
+            var p = new Prob1512();
+            var result = p.NumIdenticalPairs(new int[]{1,2,3});
+            var actual = 0;
+
+            Assert.AreEqual(actual, result);
+        }
+    }
+}
