2acbd0b13e6777b5ea0da13604666e2c3069658f
Lillian <fleming.lillian@gmail.com>
2020-09-16 21:30:43 -0500

    Stubbing out tests

---
+++ LeetCode.Code/Arrays/Prob977.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Code.Arrays
+{
+    public class Prob977
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode.Tests/ArraysTests/Prob977Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Code.Arrays;
+
+namespace LeetCode.Tests.ArraysTests
+{
+    [TestClass]
+    public class Prob977Test
+    {
+        //Input: [-4,-1,0,3,10]
+        //Output: [0,1,9,16,100]
+
+        [TestMethod]
+        public void It_should_return_0_1_9_16_100()
+        {
+
+        }
+
+        //Input: [-7,-3,2,3,11]
+        //Output: [4,9,9,49,121]
+        [TestMethod]
+        public void It_should_return_4_9_9_49_121()
+        {
+
+        }
+    }
+}
